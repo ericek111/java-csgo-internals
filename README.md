a1aef37a15609706fcdb8d875115bceaaf3ce8f1
Erik Bročko <eb.skola@gmail.com>
2018-04-14 15:01:19 +0200

    Build, load, README

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Heavily based on my [external Java cheat](https://github.com/ericek111/java-csgo
 - optimisation (Callback methods spam threads?)
 - fix JVM? The cheat can't be un/reloaded because of [JDK-4093633](https://bugs.openjdk.java.net/browse/JDK-4093633)
 - port [BufferStruct](https://github.com/ericek111/java-csgo-internals/blob/master/src/eu/lixko/csgoshared/util/BufferStruct.java) to Java 9 - *newInstance() in Class has been deprecated*
+
+### Credits:
+- [aixxe](https://aixxe.net/) for his very educative tutorials, [VMThook](https://github.com/aixxe/vmthook/blob/master/vmthook.h) and [CS:GO internal bhop](https://github.com/aixxe/csgo-bhop-linux) which I based my cheat on.
--- build.sh
+++ build.sh
@@ -5,5 +5,11 @@ JAVA_SRC_DIR="$ROOTDIR/src"
 BUILD_DIR="$ROOTDIR/bin"
 JAVA_RES_JARS=$(JARS=("$ROOTDIR/res"/*.jar); IFS=:; echo "${JARS[*]}")
 
+# Allow Eclipse hot-swap:
+BUILD_DIR="$ROOTDIR/../eclipse/CSGOInternals/bin"
+
 javac -cp "$JAVA_SRC_DIR:$ROOTDIR/res/*" -d "$BUILD_DIR/java/" "$JAVA_SRC_DIR/eu/lixko/csgointernals/Main.java" -Xlint:deprecation
-gcc -fPIC -I"$JVM_ROOT/include" -I"$JVM_ROOT/include/linux" -DJAVA_CP="\"$ROOTDIR/res/:$JAVA_RES_JARS\"" -shared -o "$BUILD_DIR/libjvmloader.so" "$ROOTDIR/jvm_loader.cpp"
+
+# link directly to file - CS:GO doesn't know where to look for libjvm.so without /etc/ld.so.conf.d/ entry or LD_LIBRARY_PATH
+# if you're okay with that, append -L"$JVM_ROOT/lib/server/" and -ljvm to below command, since it's a better practice anyway
+gcc -fPIC -I"$JVM_ROOT/include/" -I"$JVM_ROOT/include/linux/" -DJAVA_CP="\"$ROOTDIR/res/:$JAVA_RES_JARS\"" -shared -o "$BUILD_DIR/libjvmloader.so" "$ROOTDIR/jvm_loader.cpp" "$JVM_ROOT/lib/server/libjvm.so"
