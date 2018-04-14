7832913c9b384ab0c30eb61a09ccc1f31b4c549f
Erik Bročko <eb.skola@gmail.com>
2018-04-14 21:28:38 +0200

    Fixed build script... again.

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ JAVA_CLASS_DIR="$BUILD_DIR/java"
 JAVA_RES_JARS=$(JARS=("$ROOTDIR/res"/*.jar); IFS=:; echo "${JARS[*]}")
 
 # Allow Eclipse hot-swap:
-JAVA_CLASS_DIR="$ROOTDIR/../eclipse/CSGOInternals/bin"
+JAVA_CLASS_DIR="$ROOTDIR/../../eclipse/CSGOInternals/bin"
+JAVA_CLASS_DIR="$(realpath "$JAVA_CLASS_DIR")"
 
 javac -cp "$JAVA_SRC_DIR:$ROOTDIR/res/*" -d "$BUILD_DIR/java/" "$JAVA_SRC_DIR/eu/lixko/csgointernals/Main.java"
-g++ -fPIC -shared -Wl,--no-as-needed,-rpath,"$JVM_ROOT/lib/server/" -I"$JVM_ROOT/include/" -I"$JVM_ROOT/include/linux/" -L"$JVM_ROOT/lib/server/" -DJAVA_CP="\"$ROOTDIR/res/:$JAVA_RES_JARS\"" -lpthread -ldl -ljvm -o "$BUILD_DIR/libjvmloader.so" "$ROOTDIR/jvm_loader.cpp"
+g++ -fPIC -shared -Wl,--no-as-needed,-rpath,"$JVM_ROOT/lib/server/" -I"$JVM_ROOT/include/" -I"$JVM_ROOT/include/linux/" -L"$JVM_ROOT/lib/server/" -DJAVA_CP="\"$ROOTDIR/res/:$JAVA_RES_JARS:$JAVA_CLASS_DIR\"" -lpthread -ldl -ljvm -o "$BUILD_DIR/libjvmloader.so" "$ROOTDIR/jvm_loader.cpp"
